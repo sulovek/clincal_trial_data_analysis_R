345ec04c4d152dc79cba6f5c18dc61ece49804b3
sulovek <sulovekoirala@gmail.com>
2023-03-29 21:19:01 -0600

    Update survival_analysis_code.R

--- survival_analysis_code.R
+++ survival_analysis_code.R
@@ -1,7 +1,6 @@
 library(readxl)
 library(survival)
 library(survminer)
-library(ggthemes)
 
 library(readxl)
 survival <- read.csv("https://raw.githubusercontent.com/sulovek/clincal_trial_data_analysis_R/main/Dataset/survival_analysis_data.csv")
@@ -10,6 +9,6 @@ fit<-survfit(Surv(Time,Death)~Group,data=survival)
 summary(fit)
 
 ggsurvplot(fit, title = "Kaplan-Meier Survival Curves", pval = T, size = 2.3, xlab = "Days",risk.table = F,
-           ggtheme = theme_pander(base_family = "Arial", base_size = 26),
+           ggtheme = theme_base(base_family = "Arial", base_size = 26),
            font.family = "Arial")
 
